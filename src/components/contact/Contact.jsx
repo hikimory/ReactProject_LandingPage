94e6bde3645fb908ad10d1bb624c8fd38da9c418
Hikimory <nikita.bikon@gmail.com>
2023-02-08 11:11:58 +0200

    Add EmailJS library

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs-com';
 import './Contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
@@ -6,6 +7,21 @@ import {BsWhatsapp} from 'react-icons/bs'
 
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
+      e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -32,7 +48,7 @@ const Contact = () => {
         </article>
       </div>
       {/* End Of Contact Option */}
-      <form action="">
+      <form ref={form} onSubmit={sendEmail}>
         <input type="text" name='name' placeholder='Your Full Name' required/>
         <input type="email" name='email' placeholder='Your Email' required/>
         <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
